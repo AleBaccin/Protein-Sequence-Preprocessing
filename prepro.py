578527e57a41444f207362bc933ba6dff35b149c
AleBaccin <baccin.alessandro@gmail.com>
2020-05-09 16:20:19 +0100

    v1.0

--- prepro.py
+++ prepro.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import random
 import re
+import json
+import os
+from sklearn.model_selection import KFold
 from pprint import pprint
 from tqdm import tqdm
 from pathlib import Path
@@ -18,34 +21,52 @@ title = r"""
 
 instructions = """
 How to:
-prepro.py 
-    -e <inputfile> extract all the localization labels with the respective counts and also create a .tab file contained the cleaning dataset
-    -f <inputfile> given a cleaned dataset in .tab format, produce the .fasta file
-    -a <inputfile> analyze a .fasta file counting the number of entries per class
-    -l <inputfile> flatten a fasta, file converging every sequence to one liners, used to simplify encoding operations
-    -d <inputfile> generate the .dataset encoding given a .fasta file
-    --m <inputfolder> generate the .dataset encodings given a folder containing MSAs files.
+prepro.py
+    -l | --lfile    <inputfile>     Extract, save and visualize all the present labels.
+    -o | --model    <modelname>     Specify a model contained in settings.json
+    -e | --efile    <inputfile>     Create a .tab file containing sequences of classes specified in the required model. 
+    -f | --ffile    <inputfile>     Given a cleaned dataset in .tab format, produce the .fasta file.
+    -a | --ffile    <inputfile>     Analyze a .fasta file counting the number of entries per class.
+    -d | --afile    <inputfile>     Generate the three .dataset encoding files given a .fasta file
+                                    Present MSAs are also taken into consideration, and sequences not preseting the MSA information are binned.
+                                    Ouputs are stored into the generated "NoMSAdataset" folder.
+    -m | --mfile                    Given the folder containing MSAs files specified in the settings.json, attach the MSA information to the .dataset files (new files are created).
+                                    Ouputs are stored into the generated "MSAdataset" folder.
 """
 
 dotdataset_encoding_positions = {'M': 0, 'I': 1, 'S': 2, 'F': 3, 'L': 4, 
 'N': 5, 'C': 6, 'U': 6, 'V': 7, 'D': 8, 'H': 9, 
-'G': 10, 'Y': 11, 'K': 12, 'P': 13, 'Q': 14, 
+'G': 10, 'Y': 11, 'K': 12, 'P': 13, 'Q': 14,
 'W': 15, 'A': 16, 'E': 17, 'R': 18, 'T': 19, 
 'B': 20, 'J': 20, 'Z': 20, 'X': 21}
-check = ['Cytoplasm', 'Nucleus', 'Secreted', 'Cell membrane', 'Plastid', 'Mitochondrion', 'Endoplasmic reticulum membrane']
-fasta_encoding_lables = {'Cytoplasm': 1, 'Nucleus': 2, 'Secreted': 3, 'Cell membrane': 4, 'Plastid': 5, 'Mitochondrion': 6, 'Endoplasmic reticulum membrane': 7, 'Other': 8}
-fasta_decoding_lables = {'1': 'Cytoplasm', '2': 'Nucleus', '3': 'Secreted', '4': 'Cell membrane', '5': 'Plastid', '6': 'Mitochondrion', '7':'Endoplasmic reticulum membrane', '8': 'Other'}
 sub_loc = 'Subcellular location [CC]'
 eco_269 = 'ECO:0000269'
+n_letter_encoding_digits = 22
+random.seed(41)
+
+with open('settings.json', 'r') as json_file:
+  data = json.load(json_file)
+
+msas= data['msas_dir']
+output_dir = data['model_one']['outputdir']
+fasta_encoding_lables = data['model_one']['labels']
+check = list(fasta_encoding_lables.keys())[:-1]
+
+fasta_decoding_lables = {}
+for k, v in fasta_encoding_lables.items():
+    if str(v) in fasta_decoding_lables:
+        fasta_decoding_lables[str(v)] += f' / {k}'
+    else:
+        fasta_decoding_lables[str(v)] = k
 
 def line_count(inputfile):
-    return len(open(inputfile).readlines(  ))
+    return len(open(f'{output_dir}/{inputfile}').readlines())
 
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
 def clean(inputfile):
-    df = pd.read_csv(inputfile, sep='\t')
+    df = pd.read_csv(f'{output_dir}/{inputfile}', sep='\t')
     df[sub_loc].replace('', np.nan, inplace=True)
     df.dropna(subset=[sub_loc], inplace=True)
     df[sub_loc] = df[sub_loc].map(lambda x: re.split('Note=', x)[0])
@@ -67,10 +88,17 @@ def label_extraction(df):
             if eco_269 in string:
                 string = re.sub('{[^}]+}', '', string)
                 s = re.split(', ', string)
+            else:
+                continue
+
             if s[0] in check:
                 df.at[c, sub_loc] = s[0]
             else:
-                df.at[c, sub_loc] = 'Other'
+                if 'Other' in fasta_encoding_lables.keys():
+                    df.at[c, sub_loc] = 'Other'
+                else:
+                    df.drop(c, inplace = True)
+                
             if s[0] in dict:
                 dict[s[0]] = dict[s[0]] + 1
             else:
@@ -78,17 +106,13 @@ def label_extraction(df):
             break
     c += 1
     listoflabels = [(k, v) for k, v in sorted(dict.items(), key=lambda x: x[1], reverse=True)]
+
     return listoflabels, df
 
-def save_outputs_of_extraction(listoflabels, df):
-    df.to_csv('datasets/cooked.tab', columns = ['Entry', 'Sequence', 'Length', 'Subcellular location [CC]'], sep ='\t')
-    with open('datasets/labels_extracted.txt', 'w') as f:
-        for t in listoflabels:
-            f.write(' '.join(str(s) for s in t) + '\n')
-
 def fasta_encoding(inputfile):
-    df = pd.read_csv(inputfile, sep='\t')
-    f= open("datasets/entries.fasta","w+")
+    filename = inputfile.split('.')[0]
+    df = pd.read_csv(f'{output_dir}/{inputfile}', sep='\t')
+    f= open(f'{output_dir}/entries_{filename}.fasta','w+')
 
     c = 0
     for Entry, Sequence, Sub_loc in tqdm(zip(df.Entry, df.Sequence, df['Subcellular location [CC]']), total=df.shape[0]):
@@ -103,7 +127,7 @@ def analyze_fasta(inputfile):
 
     l = line_count(inputfile)
 
-    with open(inputfile) as f:
+    with open(f'{output_dir}/{inputfile}') as f:
         protein_count = 0
         for line in tqdm(f, total=l):
             if line[0] == '>':
@@ -119,83 +143,108 @@ def analyze_fasta(inputfile):
     print(listoflabels)
 
 def flatten_fasta_to_file(inputfile):
-    ff= open("datasets/flattened_entries.fasta","w+")
-
+    print("+-----Flattening-Fasta----+")
     l = line_count(inputfile)
 
-    with open(inputfile) as f:
+    ff = ""
+
+    with open(f'{output_dir}/{inputfile}') as f:
         flattened_sequence = ''
         for line in tqdm(f, total=l):
             if line[0] == '>':
                 if flattened_sequence != '':
                     flattened_sequence += '\n'
 
-                ff.write(flattened_sequence + line)
+                ff += flattened_sequence + line
                 flattened_sequence = ''
             else:
                 flattened_sequence += line.replace('\n', '')
-        ff.write(flattened_sequence)
+        ff += flattened_sequence
 
-def add_msas(inputfolder):
+    return ff
+
+def add_msas():
     files = ['train.dataset', 'test.dataset', 'validation.dataset']
 
     for fs in files:
         print(f'Clipping MSA to {fs}')
-        with open(f'datasets/NoMSAdataset/{fs}', 'r') as f_dataset_nomsa:
-            f_dataset_msa = open(f'datasets/MSAdataset/{fs}','w+')
-            l = line_count(f'datasets/NoMSAdataset/{fs}')
+        with open(f'{output_dir}/NoMSAdataset/{fs}', 'r') as f_dataset_nomsa:
+            f_dataset_msa = open(f'{output_dir}/MSAdataset/{fs}','w+')
+            l = line_count(f'NoMSAdataset/{fs}')
             count = 1
             sequence = ''
-            f_dataset_msa.write(f'{int(l / 4)}\n22 8\n')
-            for line in tqdm(f_dataset_nomsa, total=l):
+            missed = 0
+
+            f_dataset_msa.write(f'{int((l - 2) * 0.25)}\n{n_letter_encoding_digits} {len(fasta_decoding_lables)}\n')
+            for line in tqdm(f_dataset_nomsa.readlines()[2:], total=l):
                 if count % 4 == 1:
                     sequence = line.rstrip('\n')
-
                 if count % 4 == 3:
-                    msa_sequence = Path(f'{inputfolder}\{sequence}.dataset')
+                    msa_sequence = Path(f'{msas}\\{sequence}.dataset')
                     if msa_sequence.is_file():
-                        msa_sequence = open(f'{inputfolder}\{sequence}.dataset')
+                        msa_sequence = open(f'{msas}\\{sequence}.dataset')
                         lines = msa_sequence.readlines()
                         f_dataset_msa.write(lines[4])
                         msa_sequence.close()
                     else:
-                        f_dataset_msa.write('placeholder\n')
+                        missed += 1
                 else:
-                    f_dataset_msa.write(line)
+                    msa_sequence = Path(f'{msas}\\{sequence}.dataset')
+                    if msa_sequence.is_file():
+                        f_dataset_msa.write(line)
                 count += 1
+
+            f_dataset_msa.seek(0, 0)
+            f_dataset_msa.write(f'{int((l - 2)  * 0.25 - missed)}\n')
             f_dataset_msa.close()
         f_dataset_nomsa.close()
 
-def encode_flattened_fasta(inputfile):
-    train = open("datasets/NoMSAdataset/train.dataset","w+")
-    test = open("datasets/NoMSAdataset/test.dataset","w+")
-    val = open("datasets/NoMSAdataset/validation.dataset","w+")
+def encode_flattened_fasta(flattened_fasta):
+    train = open(f'{output_dir}/NoMSAdataset/train.dataset','w+')
+    test = open(f'{output_dir}/NoMSAdataset/test.dataset','w+')
+    val = open(f'{output_dir}/NoMSAdataset/validation.dataset','w+')
+    sets = [1, 2, 3]
+    weights = [0.80, 0.10, 0.10]
 
-    l = line_count(inputfile)
+    label = 0
+    rdn = random.choices(sets, weights)[0]
 
-    with open(inputfile) as f:
-        label = 0
-        rdn = 0
-        for line in tqdm(f, total=l):
-            if rdn < 3 and rdn > -1:
-                ff = train
-            elif rdn < 4 and rdn > 2:
-                ff = test
-            else:
-                ff = val
+    train_count =  0
+    val_count =  0
+    test_count =  0
 
-            if line[0] == '>':
-                line = line.split('|')
-                label = re.search(r"\((\w+)\)", line[2]).group(1)
+    rough_train_size = int(len(flattened_fasta.splitlines()) * 0.5 * 0.8)
+    rough_test_size = int(len(flattened_fasta.splitlines()) * 0.5 * 0.1)
+    rough_val_size = int(len(flattened_fasta.splitlines()) * 0.5 * 0.1)
+
+    train.write(f'{rough_train_size}\n{n_letter_encoding_digits} {len(fasta_decoding_lables)}\n')
+    test.write(f'{rough_test_size}\n{n_letter_encoding_digits} {len(fasta_decoding_lables)}\n')
+    val.write(f'{rough_val_size}\n{n_letter_encoding_digits} {len(fasta_decoding_lables)}\n')
+    msa_sequence = Path(msas)
+    for line in tqdm(flattened_fasta.splitlines(), total=len(flattened_fasta.splitlines())):
+        if rdn == 1:
+            ff = train
+        elif rdn == 2:
+            ff = val
+        elif rdn == 3:
+            ff = test
+
+        if line[0] == '>':
+            line = line.split('|')
+            label = re.search(r"\((\w+)\)", line[2]).group(1)
+            msa_sequence = Path(msas)
+            msa_sequence = Path(msa_sequence, f'{line[1]}.dataset')
+            if msa_sequence.is_file():
                 ff.write(line[1] + '\n')
-            else:
-                to_write = ''
-                line = line.rstrip('\n')
-                to_write += str(len(line)) + '\n'
-                encoding = ""
-                
+        else:
+            to_write = ''
+            line = line.rstrip('\n')
+            to_write += str(len(line)) + '\n'
+            encoding = ""
+            
+            if msa_sequence.is_file():
                 for c in line:
-                    for n in range(22):
+                    for n in range(n_letter_encoding_digits):
                         if dotdataset_encoding_positions[c] == n:
                             encoding += '1 '
                         else:
@@ -205,46 +254,91 @@ def encode_flattened_fasta(inputfile):
                 to_write += encoding + '\n'
                 to_write += label + '\n'
                 ff.write(to_write)
-                rdn = random.randrange(5)
 
-def main(argv):
-    print(title)
-    inputfile = ''
-    try:
-        opts, _ = getopt.getopt(argv,"he:f:c:a:l:d:m:",["ifile=", "ffile=", "afile=", "lfile=", "dfile=", "mfile=" ""])
-    except getopt.GetoptError:
+                if rdn == 1:
+                    train_count += 1
+                elif rdn == 2:
+                    val_count += 1
+                elif rdn == 3:
+                    test_count += 1
+
+            rdn = random.choices(sets, weights)[0]
+
+    train.seek(0, 0)
+    train.write(f'{int(train_count)}\n')
+    train.close()
+
+    test.seek(0, 0)
+    test.write(f'{int(test_count)}\n')
+    test.close()
+
+    val.seek(0, 0)
+    val.write(f'{int(val_count)}\n')
+    val.close()
+
+argv = sys.argv[1:]
+print(title)
+inputfile = ''
+try:
+    opts, _ = getopt.getopt(argv,"ho:l:e:f:a:d:m",["model=", "lfile=","efile=", "ffile=", "afile=", "dfile=", "mfile="])
+except getopt.GetoptError:
+    print(instructions)
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
         print(instructions)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(instructions)
-            sys.exit()
-        elif opt in ("-e", "--ifile"):
-            print("+----Extracting-Labels----+")
-            inputfile = arg
-            labels, df = label_extraction(clean(inputfile))
-            save_outputs_of_extraction(labels, df)
-        elif opt in ("-f", "--ffile="):
-            print("+----Encoding-to-Fasta----+")
-            inputfile = arg
-            fasta_encoding(inputfile)
-        elif opt in ("-a", "--afile="):
-            print("+-----Analyzing-Fasta-----+")
-            inputfile = arg
-            analyze_fasta(inputfile)
-        elif opt in ("-l", "--lfile="):
-            print("+-----Flattening-Fasta----+")
-            inputfile = arg
-            flatten_fasta_to_file(inputfile)
-        elif opt in ("-d", "--dfile="):
-            print("+--dotdataset-from-Fasta--+")
-            inputfile = arg
-            encode_flattened_fasta(inputfile)
-        elif opt in ("-m", "--mfil="):
-            print("+--Clipping MSA to dotdataset--+")
-            inputfolder = arg
-            add_msas(inputfolder)
+        sys.exit()
+    elif opt in ("-o", "--model"):
+        model = arg
+        output_dir = data[model]['outputdir']
+        if not os.path.exists(os.path.join(output_dir, 'MSAdataset')):
+            os.mkdir(os.path.join(output_dir, 'MSAdataset'))
+        if not os.path.exists(os.path.join(output_dir, 'NoMSAdataset')):
+            os.mkdir(os.path.join(output_dir, 'NoMSAdataset'))
 
+        fasta_encoding_lables = data[model]['labels']
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+        if 'Other' in fasta_encoding_lables.keys():
+            check = list(fasta_encoding_lables.keys())[:-1]
+        else:
+            check = list(fasta_encoding_lables.keys())
+
+        fasta_decoding_lables = {}
+        for k, v in fasta_encoding_lables.items():
+            if str(v) in fasta_decoding_lables:
+                fasta_decoding_lables[str(v)] += f' / {k}'
+            else:
+                fasta_decoding_lables[str(v)] = k
+
+        print(f'+---Working on {output_dir}---+')
+    elif opt in ("-l", "--lfile"):
+        print("+----Extracting-Labels----+")
+        inputfile = arg
+        labels, _ = label_extraction(clean(inputfile))
+        print(labels)
+        with open(f'{output_dir}/labels_extracted.txt', 'w') as f:
+            for t in labels:
+                f.write(' '.join(str(s) for s in t) + '\n')
+    elif opt in ("-e", "--efile"):
+        print("+----Extracting-reduced-.tab----+")
+        inputfile = arg
+        _, df = label_extraction(clean(inputfile))
+
+        filename = inputfile.split('.')[0]
+        df.to_csv(f'{output_dir}/cooked_{filename}.tab', columns = ['Entry', 'Sequence', 'Length', 'Subcellular location [CC]'], sep ='\t')
+    elif opt in ("-f", "--ffile="):
+        print("+----Encoding-to-Fasta----+")
+        inputfile = arg
+        fasta_encoding(inputfile)
+    elif opt in ("-a", "--afile="):
+        print("+-----Analyzing-Fasta-----+")
+        inputfile = arg
+        analyze_fasta(inputfile)
+    elif opt in ("-d", "--dfile="):
+        print("+--dotdataset-from-Fasta--+")
+        inputfile = arg
+        encode_flattened_fasta(flatten_fasta_to_file(inputfile))
+    elif opt in ("-m", "--mfile="):
+        print("+--Clipping MSA to dotdataset--+")
+        inputfolder = arg
+        add_msas()
